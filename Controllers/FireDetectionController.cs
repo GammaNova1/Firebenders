dc8624f286760dd6bba30bd9fb68c966edad3e37
Ahmet Kapusız <ahmetkpz70@gmail.com>
2024-05-25 02:32:24 +0300

    Çalışan model değişti artık firebenders_deploy modelindeyiz. homecontroller açıldığı gibi indexte benim amazondan çekilen fotoğraflarla bir tahmin yapıyor. Eğer kullanıcı isterse kendisi de upload yapabiliyor.

---
+++ Controllers/FireDetectionController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Firebenders.Controllers
+{
+    public class FireDetectionController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public FireDetectionController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FirePredict()
+        {
+            string baseUrl = "https://firebenders.s3.eu-central-1.amazonaws.com/firebenders/";
+            string[] categories = { "fire", "not_fire" };
+
+            // Rastgele bir kategori (fire veya not_fire) seç
+            Random rnd = new Random();
+            string category = categories[rnd.Next(categories.Length)];
+
+            // Rastgele bir sayı seç (0 ile 49 arasında)
+            int number = rnd.Next(0, 50);
+            string numberString = number.ToString("D3");
+
+            // Resim URL'sini oluştur
+            string imageUrl = baseUrl + $"{category}{numberString}.png";
+
+            try
+            {
+                
+                // Resmi al
+                var imageResponse = await _httpClient.GetAsync(imageUrl);
+                imageResponse.EnsureSuccessStatusCode();
+
+                // Resmi Flask API'ye gönder ve tahmin al
+                var requestBody = new { url = imageUrl };
+                var predictionResponse = await _httpClient.PostAsJsonAsync("http://localhost:5000/predict", requestBody);
+                predictionResponse.EnsureSuccessStatusCode();
+
+                var jsonResponse = await predictionResponse.Content.ReadAsStringAsync();
+                var prediction = JObject.Parse(jsonResponse)["predicted_label"].ToString();
+
+                ViewBag.Prediction = prediction;
+                ViewBag.ImageUrl = imageUrl;
+
+                return View("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = $"Error: {ex.Message}";
+                return View("Hata");
+            }
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,10 +14,62 @@ namespace Firebenders.Controllers
             _httpClient = httpClient;
         }
 
+        //[HttpGet]
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                string baseUrl = "https://firebenders.s3.eu-central-1.amazonaws.com/firebenders/";
+                string[] categories = { "fire", "not_fire" };
+
+                // Rastgele bir kategori (fire veya not_fire) seç
+                Random rnd = new Random();
+                string category = categories[rnd.Next(categories.Length)];
+
+                // Rastgele bir sayı seç (0 ile 49 arasında)
+                int number = rnd.Next(0, 50);
+                string numberString = number.ToString("D3");
+
+                // Resim URL'sini oluştur
+                string imageUrl = baseUrl + $"{category}{numberString}.png";
+
+                // Resmi al
+                var imageResponse = await _httpClient.GetAsync(imageUrl);
+                imageResponse.EnsureSuccessStatusCode();
+
+                // Resmi Flask API'ye gönder ve tahmin al
+                var requestBody = new { url = imageUrl };
+                var predictionResponse = await _httpClient.PostAsJsonAsync("http://localhost:5000/predicturl", requestBody);
+                predictionResponse.EnsureSuccessStatusCode();
+
+                var jsonResponse = await predictionResponse.Content.ReadAsStringAsync();
+                var prediction = JObject.Parse(jsonResponse)["predicted_label"].ToString();
+
+                ViewBag.Prediction2 = prediction;
+                ViewBag.ImageUrl = imageUrl;
+                if (prediction.ToLower() == "fire")
+                {
+                    Random rand = new Random();
+                    double latitude = rand.NextDouble() * (42.1071 - 36.0) + 35.0;
+                    double longitude = rand.NextDouble() * (44.7931 - 26.0) + 26.0;
+
+                    ViewBag.Latitude2 = latitude;
+                    ViewBag.Longitude2 = longitude;
+                }
+
+                return View("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = $"Error: {ex.Message}";
+                return View("Hata");
+            }
         }
 
         [HttpPost]
@@ -29,35 +81,44 @@ namespace Firebenders.Controllers
                 return View("Index");
             }
 
-            var filePath = Path.GetTempFileName();
-            using (var stream = System.IO.File.Create(filePath))
+            try
             {
-                await image.CopyToAsync(stream);
+                var filePath = Path.GetTempFileName();
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await image.CopyToAsync(stream);
+                }
+
+                // Send the image to the Flask API
+                var form = new MultipartFormDataContent();
+                form.Add(new StreamContent(System.IO.File.OpenRead(filePath)), "img", image.FileName);
+
+                var response = await _httpClient.PostAsync("http://localhost:5000/predictpath", form);
+                response.EnsureSuccessStatusCode();
+
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var prediction = JObject.Parse(jsonResponse)["prediction"].ToString();
+
+                ViewBag.Prediction = prediction;
+
+                if (prediction.ToLower() == "fire")
+                {
+                    Random rand = new Random();
+                    double latitude = rand.NextDouble() * (42.1071 - 36.0) + 35.0;
+                    double longitude = rand.NextDouble() * (44.7931 - 26.0) + 26.0;
+
+                    ViewBag.Latitude = latitude;
+                    ViewBag.Longitude = longitude;
+                }
+
+                return View("Index");
             }
-
-            // Send the image to the Flask API
-            var form = new MultipartFormDataContent();
-            form.Add(new StreamContent(System.IO.File.OpenRead(filePath)), "img", image.FileName);
-
-            var response = await _httpClient.PostAsync("http://localhost:5000/prediction", form);
-            response.EnsureSuccessStatusCode();
-
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            var prediction = JObject.Parse(jsonResponse)["prediction"].ToString();
-
-            ViewBag.Prediction = prediction;
-
-            if (prediction.ToLower() == "fire")
+            catch (Exception ex)
             {
-                Random rand = new Random();
-                double latitude = rand.NextDouble() * (42.1071 - 36.0) + 35.0;
-                double longitude = rand.NextDouble() * (44.7931 - 26.0) + 26.0;
-
-                ViewBag.Latitude = latitude;
-                ViewBag.Longitude = longitude;
+                ViewBag.Error = $"Error: {ex.Message}";
+                return View("Hata");
             }
-
-            return View("Index");
         }
+
     }
 }
