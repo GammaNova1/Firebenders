439eeada8b804e0306eab70371e15cf7452cdeea
Ahmet Kapusız <ahmetkpz70@gmail.com>
2024-06-04 17:48:09 +0300

    push

--- Controllers/FireDetectionController.cs
+++ Controllers/FireDetectionController.cs
@@ -1,119 +1,120 @@
 ﻿using Firebenders.Data;
 using Firebenders.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json.Linq;
 
-namespace Firebenders.Controllers
+public class FireDetectionController : Controller
 {
-    public class FireDetectionController : Controller
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ApplicationDbContext _context;
+    private readonly IMemoryCache _cache;
+
+    public FireDetectionController(IHttpClientFactory httpClientFactory, ApplicationDbContext context, IMemoryCache cache)
     {
-        private readonly HttpClient _httpClient;
-        private readonly ApplicationDbContext _context;
+        _httpClientFactory = httpClientFactory;
+        _context = context;
+        _cache = cache;
+    }
 
-        public FireDetectionController(HttpClient httpClient, ApplicationDbContext context)
+    [HttpGet]
+    public IActionResult Anasayfa()
+    {
+        return View("Anasayfa");
+    }
+
+    [HttpGet]
+    public IActionResult Arsiv()
+    {
+        if (!_cache.TryGetValue("Records", out List<Records> records))
         {
-            _httpClient = httpClient;
-            _context = context;
+            records = _context.Records.ToList();
+            _cache.Set("Records", records, TimeSpan.FromMinutes(5)); // Cache veriyi 5 dakika saklar
         }
+        return View(records);
+    }
 
-        [HttpGet]
-        public IActionResult Anasayfa()
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        try
         {
-            return View("Anasayfa");
-        }
+            var httpClient = _httpClientFactory.CreateClient();
+            string baseUrl = "https://firebenders.s3.eu-central-1.amazonaws.com/firebenders/";
+            string[] categories = { "fire", "not_fire" };
 
-        [HttpGet]
-        public IActionResult Arsiv()
-        {
-             var records = _context.Records.ToList();
-            return View(records);
-        }
+            Random rnd = new Random();
+            string category = categories[rnd.Next(categories.Length)];
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            try
+            int number = rnd.Next(0, 50);
+            string numberString = number.ToString("D3");
+            string imgName = $"{category}{numberString}.png";
+            ViewBag.imgName = imgName;
+            string imageUrl = baseUrl + imgName;
+
+            var imageResponse = await httpClient.GetAsync(imageUrl);
+            imageResponse.EnsureSuccessStatusCode();
+
+            var requestBody = new { url = imageUrl };
+            var predictionResponse = await httpClient.PostAsJsonAsync("http://localhost:5000/predicturl", requestBody);
+            predictionResponse.EnsureSuccessStatusCode();
+
+            var jsonResponse = await predictionResponse.Content.ReadAsStringAsync();
+            var prediction = JObject.Parse(jsonResponse)["predicted_label"].ToString();
+
+            ViewBag.Prediction2 = prediction;
+            ViewBag.ImageUrl = imageUrl;
+
+            double latitude = 0, longitude = 0;
+
+            if (prediction.ToLower() == "fire")
             {
-                string baseUrl = "https://firebenders.s3.eu-central-1.amazonaws.com/firebenders/";
-                string[] categories = { "fire", "not_fire" };
+                Random rand = new Random();
+                double minLatitude = 37.0; // Türkiye'nin güney sınırı
+                double maxLatitude = 41.0; // Türkiye'nin kuzey sınırı
+                double minLongitude = 28.0; // Türkiye'nin batı sınırı
+                double maxLongitude = 43.0; // Türkiye'nin doğu sınırı
 
-                Random rnd = new Random();
-                string category = categories[rnd.Next(categories.Length)];
+                latitude = rand.NextDouble() * (maxLatitude - minLatitude) + minLatitude;
+                longitude = rand.NextDouble() * (maxLongitude - minLongitude) + minLongitude;
 
-                int number = rnd.Next(0, 50);
-                string numberString = number.ToString("D3");
-                string imgName = $"{category}{numberString}.png";
-                ViewBag.imgName = imgName;
-                string imageUrl = baseUrl + imgName;
-
-                var imageResponse = await _httpClient.GetAsync(imageUrl);
-                imageResponse.EnsureSuccessStatusCode();
-
-                var requestBody = new { url = imageUrl };
-                var predictionResponse = await _httpClient.PostAsJsonAsync("http://localhost:5000/predicturl", requestBody);
-                predictionResponse.EnsureSuccessStatusCode();
-
-                var jsonResponse = await predictionResponse.Content.ReadAsStringAsync();
-                var prediction = JObject.Parse(jsonResponse)["predicted_label"].ToString();
-
-                ViewBag.Prediction2 = prediction;
-                ViewBag.ImageUrl = imageUrl;
-
-                double latitude = 0, longitude = 0;
-
-                if (prediction.ToLower() == "fire")
+                ViewBag.Latitude = latitude;
+                ViewBag.Longitude = longitude;
+                var fireRecord = new Records
                 {
-                    Random rand = new Random();
-                    double minLatitude = 37.0; // Türkiye'nin güney sınırı
-                    double maxLatitude = 41.0; // Türkiye'nin kuzey sınırı
-                    double minLongitude = 28.0; // Türkiye'nin batı sınırı
-                    double maxLongitude = 43.0; // Türkiye'nin doğu sınırı
-
-                    latitude = rand.NextDouble() * (maxLatitude - minLatitude) + minLatitude;
-                    longitude = rand.NextDouble() * (maxLongitude - minLongitude) + minLongitude;
-
-                    ViewBag.Latitude = latitude;
-                    ViewBag.Longitude = longitude;
-                    var fireRecord = new Records
-                    {
-                        RecordName = imgName,
-                        RecordImage = imageUrl,
-                        RecordLatitude = latitude.ToString(),
-                        RecordLongtitude = longitude.ToString(),
-                        RecordStatus = true,
-                        RecordDate = DateTime.Now,
-                        RecordStyle = true
-                    };
-                    _context.Records.Add(fireRecord);
-                    await _context.SaveChangesAsync();
-                }
-               
-                else
-                {
-                    var fireRecord = new Records
-                    {
-                        RecordName = imgName,
-                        RecordImage = imageUrl,
-                        RecordLatitude = latitude.ToString(),
-                        RecordLongtitude = longitude.ToString(),
-                        RecordStatus = false,
-                        RecordDate = DateTime.Now,
-                        RecordStyle = true
-                    };
-                    _context.Records.Add(fireRecord);
-                    await _context.SaveChangesAsync();
-                }
-                
-
-                
-
-                return View("Index");
+                    RecordName = imgName,
+                    RecordImage = imageUrl,
+                    RecordLatitude = latitude.ToString(),
+                    RecordLongtitude = longitude.ToString(),
+                    RecordStatus = true,
+                    RecordDate = DateTime.Now,
+                    RecordStyle = true
+                };
+                _context.Records.Add(fireRecord);
+                await _context.SaveChangesAsync();
             }
-            catch (Exception ex)
+            else
             {
-                ViewBag.Error = $"Error: {ex.Message}";
-                return View("Hata");
+                var fireRecord = new Records
+                {
+                    RecordName = imgName,
+                    RecordImage = imageUrl,
+                    RecordLatitude = latitude.ToString(),
+                    RecordLongtitude = longitude.ToString(),
+                    RecordStatus = false,
+                    RecordDate = DateTime.Now,
+                    RecordStyle = true
+                };
+                _context.Records.Add(fireRecord);
+                await _context.SaveChangesAsync();
             }
+
+            return View("Index");
+        }
+        catch (Exception ex)
+        {
+            ViewBag.Error = $"Error: {ex.Message}";
+            return View("Hata");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddMemoryCache();
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 
